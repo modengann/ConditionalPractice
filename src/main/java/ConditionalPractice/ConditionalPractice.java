1418d5b953462a755016cd9f99b1715e7cb39e01
modengann <99417989+modengann@users.noreply.github.com>
2022-09-09 09:30:55 -0400

    init

---
+++ src/main/java/ConditionalPractice/ConditionalPractice.java
@@ -0,0 +1,13 @@
+package ConditionalPractice;
+
+public class ConditionalPractice {
+
+    public static void main(String[] args) {
+        x = reader.nextLine();
+        if (x > 0 && x <= 10)
+            System.out.println("x is between 0 and 10 inclusive");
+        otherwise
+            System.out.println("x is either less than 0 or greater than 10");
+    }
+    
+}
